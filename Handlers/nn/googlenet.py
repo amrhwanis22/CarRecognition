48285f1f56ca3543697ba3cdb726a88aecaf19a5
amrhwanis22 <amr.h.wanis@gmail.com>
2020-01-25 20:33:24 +0200

    fixing errors

--- Handlers/nn/googlenet.py
+++ Handlers/nn/googlenet.py
@@ -27,19 +27,19 @@ class GoogleNet:
 
     @staticmethod
     def incention_module(x,num1x1,num3x3Reduce,num3x3,num5x5Reduce,num5x5,num1x1Proj,chanDim,stage,reg=0.0005):
-        first=DeeperGooLeNet.conv_module(x,num1x1,1,1,(1,1),chanDim,reg=reg,name=stage+'_first')
-        second=DeeperGooLeNet.conv_module(x,num3x3Reduce,1,1,(1,1),chanDim,reg=reg,name=stage+'_second1')
-        second=DeeperGooLeNet.conv_module(second,num3x3,3,3,(1,1),chanDim,reg=reg,name=stage+'_second2')
-        third=DeeperGooLeNet.conv_module(x,num5x5Reduce,1,1,(1,1),chanDim,reg=reg,name=stage+'_third1')
-        third=DeeperGooLeNet.conv_module(third,num5x5,5,5,(1,1),chanDim,reg=reg,name=stage+'_third2')
+        first=GoogleNet.conv_module(x,num1x1,1,1,(1,1),chanDim,reg=reg,name=stage+'_first')
+        second=GoogleNet.conv_module(x,num3x3Reduce,1,1,(1,1),chanDim,reg=reg,name=stage+'_second1')
+        second=GoogleNet.conv_module(second,num3x3,3,3,(1,1),chanDim,reg=reg,name=stage+'_second2')
+        third=GoogleNet.conv_module(x,num5x5Reduce,1,1,(1,1),chanDim,reg=reg,name=stage+'_third1')
+        third=GoogleNet.conv_module(third,num5x5,5,5,(1,1),chanDim,reg=reg,name=stage+'_third2')
         fourth=MaxPooling2D((3,3),strides=(1,1),padding='same',name=stage+'_pool')(x)
-        fourth=DeeperGooLeNet.conv_module(fourth,num1x1Proj,1,1,(1,1),chanDim,reg=reg,name=stage+'_fourth')
+        fourth=GoogleNet.conv_module(fourth,num1x1Proj,1,1,(1,1),chanDim,reg=reg,name=stage+'_fourth')
         x=concatenate([first,second,third,fourth],axis=chanDim,name=stage+'_mixed')
         return x
 
 
     @staticmethod
-    def build(width,height,depth,classes,reg=0.0005):
+    def build(width,height,depth,classes,reg=0.005):
         inputShape=(height,width,depth)
         chanDim=-1
 
@@ -48,19 +48,19 @@ class GoogleNet:
             chanDim=1
 
         inputs=Input(shape=inputShape)
-        x=DeeperGooLeNet.conv_module(inputs,64,5,5,(1,1),chanDim,reg=reg,name='block1')
+        x=GoogleNet.conv_module(inputs,64,5,5,(1,1),chanDim,reg=reg,name='block1')
         x=MaxPooling2D((3,3),strides=(2,2),padding='same',name='pool1')(x)
-        x=DeeperGooLeNet.conv_module(x,64,1,1,(1,1),chanDim,reg=reg,name='block2')
-        x=DeeperGooLeNet.conv_module(x,192,3,3,(1,1),chanDim,reg=reg,name='block3')
+        x=GoogleNet.conv_module(x,64,1,1,(1,1),chanDim,reg=reg,name='block2')
+        x=GoogleNet.conv_module(x,192,3,3,(1,1),chanDim,reg=reg,name='block3')
         x=MaxPooling2D((3,3),strides=(2,2),padding='same',name='pool2')(x)
-        x=DeeperGooLeNet.incention_module(x,64,96,128,16,32,32,chanDim,'3a',reg=reg)
-        x=DeeperGooLeNet.incention_module(x,128,128,192,32,96,64,chanDim,'3b',reg=reg)
+        x=GoogleNet.incention_module(x,64,96,128,16,32,32,chanDim,'3a',reg=reg)
+        x=GoogleNet.incention_module(x,128,128,192,32,96,64,chanDim,'3b',reg=reg)
         x=MaxPooling2D((3,3),strides=(2,2),padding='same',name='pool3')(x)
-        x=DeeperGooLeNet.incention_module(x,192,96,208,16,48,64,chanDim,'4a',reg=reg)
-        x=DeeperGooLeNet.incention_module(x,160,112,224,24,64,64,chanDim,'4b',reg=reg)
-        x=DeeperGooLeNet.incention_module(x,128,128,256,24,64,64,chanDim,'4c',reg=reg)
-        x=DeeperGooLeNet.incention_module(x,112,144,288,32,64,64,chanDim,'4d',reg=reg)
-        x=DeeperGooLeNet.incention_module(x,256,160,320,32,128,128,chanDim,'4e',reg=reg)
+        x=GoogleNet.incention_module(x,192,96,208,16,48,64,chanDim,'4a',reg=reg)
+        x=GoogleNet.incention_module(x,160,112,224,24,64,64,chanDim,'4b',reg=reg)
+        x=GoogleNet.incention_module(x,128,128,256,24,64,64,chanDim,'4c',reg=reg)
+        x=GoogleNet.incention_module(x,112,144,288,32,64,64,chanDim,'4d',reg=reg)
+        x=GoogleNet.incention_module(x,256,160,320,32,128,128,chanDim,'4e',reg=reg)
         x=MaxPooling2D((3,3),strides=(2,2),padding='same')(x)
         x=AveragePooling2D((4,4),name='pool5')(x)
         x=Dropout(0.4,name='do')(x)
